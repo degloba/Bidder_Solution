5233216b6fae6d51347aca3db5f9ed6da6c6d0f7
degloba Projects <degloba@degloba.com>
2016-12-17 20:57:52 +0100

    OK

---
+++ Domain.MainBoundedContext/MySQL/IBidderClasseRepository.vb
@@ -0,0 +1,8 @@
+﻿Imports Domain.MainBoundedContext.Modules
+
+Public Interface IBidderClasseRepository
+    Inherits Swaksoft.Domain.Seedwork.Aggregates.IRepository(Of EntityBase)
+
+    Function CreateRepository(Of TEntity As {EntityBase, New})() As Swaksoft.Domain.Seedwork.Aggregates.IRepository(Of TEntity)
+
+End Interface
--- Infrastructure.Data.MainBoundedContext.Repositories/MySql/BidderClasseRepository.vb
+++ Infrastructure.Data.MainBoundedContext.Repositories/MySql/BidderClasseRepository.vb
@@ -1,10 +1,17 @@
-﻿Imports Domain.MainBoundedContext.Modules
+﻿Imports Domain.MainBoundedContext
+Imports Domain.MainBoundedContext.Modules
 Imports Swaksoft.Domain.Seedwork
+Imports Swaksoft.Domain.Seedwork.Aggregates
 
 Public Class BidderClasseRepository
     Inherits Swaksoft.Infrastructure.Data.NHibernate.Seedwork.Repositories.Repository(Of EntityBase)
+    Implements IBidderClasseRepository
 
     Public Sub New(unitOfWork As ITransactionUnitOfWork)
         MyBase.New(unitOfWork)
     End Sub
+
+    Public Function CreateRepository(Of TEntity As {EntityBase, New})() As IRepository(Of TEntity) Implements IBidderClasseRepository.CreateRepository
+        Throw New NotImplementedException()
+    End Function
 End Class
