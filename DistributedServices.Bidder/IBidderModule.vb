a7d86ab731feccc26ab4e9b3016d2453bed85922
degloba Projects <degloba@degloba.com>
2016-12-17 20:46:52 +0100

    DDD-Seedwork (http://mdarlea.github.io/DDD-Seedwork/)

---
+++ DistributedServices.Bidder/IBidderModule.vb
@@ -0,0 +1,3 @@
+﻿Public Interface IBidderModule
+
+End Interface
---
+++ Infrastructure.Data.MainBoundedContext.Repositories/MySql/BidderClasseRepository.vb
@@ -0,0 +1,10 @@
+﻿Imports Domain.MainBoundedContext.Modules
+Imports Swaksoft.Domain.Seedwork
+
+Public Class BidderClasseRepository
+    Inherits Swaksoft.Infrastructure.Data.NHibernate.Seedwork.Repositories.Repository(Of EntityBase)
+
+    Public Sub New(unitOfWork As ITransactionUnitOfWork)
+        MyBase.New(unitOfWork)
+    End Sub
+End Class
