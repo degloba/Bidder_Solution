61a652891b1244e1f4ddad72f6c540d5090f0161
degloba Projects <degloba@degloba.com>
2016-12-17 22:29:52 +0100

    ok

--- Infrastructure.Data.MainBoundedContext.Repositories/MySql/BidderClasseRepository.vb
+++ Infrastructure.Data.MainBoundedContext.Repositories/MySql/BidderClasseRepository.vb
@@ -7,11 +7,15 @@ Public Class BidderClasseRepository
     Inherits Swaksoft.Infrastructure.Data.NHibernate.Seedwork.Repositories.Repository(Of EntityBase)
     Implements IBidderClasseRepository
 
+    Private _context As ITransactionUnitOfWork
+
     Public Sub New(unitOfWork As ITransactionUnitOfWork)
         MyBase.New(unitOfWork)
     End Sub
 
     Public Function CreateRepository(Of TEntity As {EntityBase, New})() As IRepository(Of TEntity) Implements IBidderClasseRepository.CreateRepository
-        Throw New NotImplementedException()
+        'Throw New NotImplementedException()
+        Dim rep As IRepository(Of TEntity) = New Swaksoft.Infrastructure.Data.NHibernate.Seedwork.Repositories.Repository(Of TEntity)(_context)
+        Return rep
     End Function
 End Class
