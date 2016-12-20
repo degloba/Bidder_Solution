688f3504b2bee30878eb4131f6f6dbf429d7ac91
degloba Projects <degloba@degloba.com>
2016-12-20 21:01:41 +0100

    AndroidBidder

---
+++ AndroidBidder/MainActivity.cs
@@ -0,0 +1,52 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+using Android.Gms.Common;
+
+using Firebase.Messaging;
+using Firebase.Iid;
+using Android.Util;
+
+
+namespace AndroidBidder
+{
+    [Activity(Label = "AndroidBidder", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+        TextView msgText;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            msgText = FindViewById<TextView>(Resource.Id.msgText);
+            IsPlayServicesAvailable();
+        }
+
+
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                    msgText.Text = GoogleApiAvailability.Instance.GetErrorString(resultCode);
+                else
+                {
+                    msgText.Text = "This device is not supported";
+                    Finish();
+                }
+                return false;
+            }
+            else
+            {
+                msgText.Text = "Google Play Services is available.";
+                return true;
+            }
+        }
+
+    }
+}
+
