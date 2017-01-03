78134c5ff6bb34831c755c5249d8ac844e716b0d
degloba Projects <degloba@degloba.com>
2017-01-03 20:52:20 +0100

    o

--- UI.Winforms.Controller/CBidder.vb
+++ UI.Winforms.Controller/CBidder.vb
@@ -6,4 +6,17 @@
         Return _bidder
     End Function
 
+
+    Public Function ObtenirBidder() As IList(Of signevital)
+
+        ' RestFul
+        Using client As New Net.WebClient
+
+            Dim content As String = client.DownloadString(String.Format("{0}/{1}/{2}/{3}/{4}/", "url_rest", Nothing, Nothing, Nothing))
+
+            Return Newtonsoft.Json.JsonConvert.DeserializeObject(Of List(Of signeVital))(content)
+        End Using
+
+    End Function
+
 End Class
