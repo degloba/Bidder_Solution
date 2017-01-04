fe64e823d20476d8ba60a64e2c602d1574b89a26
degloba Projects <degloba@degloba.com>
2017-01-04 20:52:29 +0100

    ok

---
+++ Domain.MainBoundedContext/Users/IUserRepository.vb
@@ -0,0 +1,13 @@
+﻿Public Interface IUserRepository
+
+#Region "User"
+
+    Function Get_NomCompletByLogin(ByVal login As String) As String
+
+    Function Get_LoginByEmployeeNumber(ByVal employeeNumber As String) As String
+
+    Function Get_ComputerInformationDescripcio(ByVal computerName As String) As String
+
+#End Region
+
+End Interface
